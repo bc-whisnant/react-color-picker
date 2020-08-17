58299cc5d5ad735e9e5619a1d6b0ccb056b42721
brandondev <brandondev@brandondevs-Mac-mini.local>
2020-08-17 18:02:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    backgroundColor: ''
+  }
+
+  changeBackgroundColor = (e) => {
+    let target = e.target.classList[0]
+  
+  switch(target) {
+    default: 
+      this.setState({
+        backgroundColor: 'black'
+      })
+      break;
+    case 'red':
+      this.setState({
+        backgroundColor: 'red'
+      })
+      break;
+    case 'green':
+      this.setState({
+        backgroundColor: '#26FF03'
+      })
+      break;
+    case 'blue':
+      this.setState({
+        backgroundColor: '#40B3FE'
+      })
+      break;
+     case 'orange':
+      this.setState({
+        backgroundColor: '#F9AE03'
+      })
+      break;
+     case 'pink':
+      this.setState({
+        backgroundColor: '#FA00FF'
+      })
+      break;
+  } 
+  }
+  render() {
+
+    let style = {
+      backgroundColor: this.state.backgroundColor
+    }
+    return (
+      <div className='App'>
+        <div className='large-swatch' style={style}></div>
+        <div className='button-container'>
+          <div className='red' onClick={this.changeBackgroundColor}></div>
+          <div className='green' onClick={this.changeBackgroundColor}></div>
+          <div className='blue' onClick={this.changeBackgroundColor}></div>
+          <div className='orange' onClick={this.changeBackgroundColor}></div>
+          <div className='pink' onClick={this.changeBackgroundColor}></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
